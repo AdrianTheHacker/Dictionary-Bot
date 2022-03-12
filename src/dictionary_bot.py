8ad9f53e45c6fda0fd8e7cf888f2d6b1ab8de68c
Gravitysause <57376726+Gravitysause@users.noreply.github.com>
2022-03-12 11:34:14 -0500

    Finishing Main Code
    Adding in the main functionality of the bot

--- src/dictionary_bot.py
+++ src/dictionary_bot.py
@@ -3,9 +3,15 @@ import discord
 import os
 import dotenv
 
+from get_definition import get_def, get_def_num
 
 class dictionary_bot(discord.Client):
-    prefix = "$"
+    prefix = "!"
+
+    def get_word(self, message):
+        wordList = message.content.split()
+
+        return wordList[1]
 
     async def on_ready(self):
         print(f"logged in as {self.user}")
@@ -21,8 +27,22 @@ class dictionary_bot(discord.Client):
         ## Debug Commands ##
         ####################
 
-        if message.content.startswith(prefix + "running"):
+        if message.content.startswith("!running"):
             await message.channel.send(f"Hello, {message.author}")
+            print(f"Hello, {message.author}")
+
+            return
+
+        
+        ###################
+        ## Main Commands ##
+        ###################
+
+        if message.content.startswith("!define"):
+            for definition in range(get_def_num(self.get_word(message))):
+                await message.channel.send(f"{definition + 1}. {get_def(self.get_word(message), definition)}")
+
+            return
 
 
 dotenv.load_dotenv()
--- src/get_definition.py
+++ src/get_definition.py
@@ -1,16 +1,27 @@
 import requests
 
 
-def get_def(word):
+def get_def(word, index):
     responce = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
     print(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
 
     meaningList = []
 
     for Homonym in range(len(responce.json())):
-        meaningList += responce.json()[Homonym]["meanings"]
+        for definition in responce.json()[Homonym]["meanings"][0]["definitions"]:
+            meaningList += [definition["definition"]]
 
-    return meaningList
+    return meaningList[index]
 
 
-print(get_def("shit"))
+def get_def_num(word):
+    responce = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+    print(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+
+    num = 0
+
+    for Homonym in range(len(responce.json())):
+        for definition in responce.json()[Homonym]["meanings"][0]["definitions"]:
+            num += 1
+
+    return num
